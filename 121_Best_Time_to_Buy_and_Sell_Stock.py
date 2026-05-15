6d484b6f449ce0c39716d93ebdb5798d25284899
viethoang4254 <hoang_2251220063@dau.edu.vn>
2026-05-15 15:36:48 +0700

    Delete 121_Best_Time_to_Buy_and_Sell_Stock.py

--- 121_Best_Time_to_Buy_and_Sell_Stock.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxProfit(self, prices):
-        min_price = float('inf')
-        best = 0
-        for p in prices:
-            min_price = min(min_price, p)
-            best = max(best, p - min_price)
-        return best
