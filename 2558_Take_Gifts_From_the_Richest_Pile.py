cbc64d0327ca084d52a0f59f7df314fc08e0b8a2
viethoang4254 <hoang_2251220063@dau.edu.vn>
2026-05-15 13:19:04 +0700

    Delete 2558_Take_Gifts_From_the_Richest_Pile.py

--- 2558_Take_Gifts_From_the_Richest_Pile.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def pickGifts(self, gifts, k):
-        import heapq
-        heap = [-g for g in gifts]
-        heapq.heapify(heap)
-        for _ in range(k):
-            x = -heapq.heappop(heap)
-            heapq.heappush(heap, -int(x ** 0.5))
-        return -sum(heap)
