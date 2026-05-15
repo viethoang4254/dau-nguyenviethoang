d7468c9b77665146b9456bd09b00ce362c35229e
viethoang4254 <hoang_2251220063@dau.edu.vn>
2026-05-15 15:37:25 +0700

    Delete 1773_Count_Items_Matching_a_Rule.py

--- 1773_Count_Items_Matching_a_Rule.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def countMatches(self, items, ruleKey, ruleValue):
-        idx = {"type": 0, "color": 1, "name": 2}[ruleKey]
-        return sum(1 for item in items if item[idx] == ruleValue)
