9e2d12ba424400e34c0434f18e5b45e6d324d8f0
viethoang4254 <hoang_2251220063@dau.edu.vn>
2026-05-15 13:18:46 +0700

    Delete 2582_Pass_the_Pillow.py

--- 2582_Pass_the_Pillow.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def passThePillow(self, n, time):
-        cycle = n - 1
-        t = time % (2 * cycle)
-        if t <= cycle:
-            return 1 + t
-        return n - (t - cycle)
