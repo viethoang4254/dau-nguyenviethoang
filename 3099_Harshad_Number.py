52e2f52e66c87cb3f51cdc926ef37b0b7bbe93f7
viethoang4254 <hoang_2251220063@dau.edu.vn>
2026-05-15 13:17:31 +0700

    Delete 3099_Harshad_Number.py

--- 3099_Harshad_Number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sumOfTheDigitsOfHarshadNumber(self, x):
-        s = 0
-        y = x
-        while y:
-            s += y % 10
-            y //= 10
-        return s if x % s == 0 else -1
