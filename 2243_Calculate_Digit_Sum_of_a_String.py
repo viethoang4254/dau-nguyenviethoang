968458a5f0362dd1751574b0c1f34d867b6ee7e2
viethoang4254 <hoang_2251220063@dau.edu.vn>
2026-05-15 13:20:26 +0700

    Delete 2243_Calculate_Digit_Sum_of_a_String.py

--- 2243_Calculate_Digit_Sum_of_a_String.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def digitSum(self, s, k):
-        while len(s) > k:
-            parts = []
-            for i in range(0, len(s), k):
-                chunk = s[i:i + k]
-                parts.append(str(sum(int(ch) for ch in chunk)))
-            s = "".join(parts)
-        return s
