c3610528025eb5aa6ca88fcd0e6d381cc12aa0be
viethoang4254 <hoang_2251220063@dau.edu.vn>
2026-05-15 13:17:50 +0700

    Delete 2974_Minimum_Number_Game.py

--- 2974_Minimum_Number_Game.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def numberGame(self, nums):
-        nums.sort()
-        res = []
-        for i in range(0, len(nums), 2):
-            res.append(nums[i + 1])
-            res.append(nums[i])
-        return res
