65e0e346a7da0a6982a279dd457c70b3c7c4ebf2
viethoang4254 <hoang_2251220063@dau.edu.vn>
2026-05-15 15:36:10 +0700

    Delete 1295_Find_Numbers_with_Even_Number_of_Digits.py

--- 1295_Find_Numbers_with_Even_Number_of_Digits.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findNumbers(self, nums):
-        count = 0
-        for x in nums:
-            if len(str(abs(x))) % 2 == 0:
-                count += 1
-        return count
